8ee4d1cfc9784d33fc528ebeba6ff905eefc33e7
itmir913 <itmir913@gmail.com>
2026-07-04 13:53:13 +0900

    fix: remove sitemap integration (build error), update lockfile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://luminousky.com',
@@ -23,5 +22,5 @@ export default defineConfig({
     },
   },
 
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
 });
